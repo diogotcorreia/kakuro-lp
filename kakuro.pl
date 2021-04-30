d7ea2d6ba0a6680ba6cd3f474296c640c27cf5b0
Diogo Correia <me@diogotc.com>
2021-04-30 19:38:54 +0100

    Add predicate espaco_fila/3
    As auxiliary predicates, soma_na_direcao/3 and
    espaco_fila/4 were also added.

--- kakuro.pl
+++ kakuro.pl
@@ -9,3 +9,30 @@ permutacoes_soma(N, Els, Soma, Perms) :-
   % TODO why doesn't setof work here?
   findall(X, (member(Comb, Combs), permutation(X, Comb)), Perms_Unsorted),
   sort(Perms_Unsorted, Perms).
+
+soma_na_direcao([_, S], h, S).
+soma_na_direcao([S, _], v, S).
+
+espaco_fila(L, espaco(N, EL), D) :-
+  espaco_fila(L, espaco(N, EL), espaco(N, []), D).
+
+espaco_fila([], espaco(N, EL), espaco(N, EL)).
+
+espaco_fila([P | _], espaco(N, EL), espaco(N, EL)) :- 
+  nonvar(P),
+  nonvar(N),
+  length(EL, Length),
+  Length > 0.
+
+espaco_fila([P | R], espaco(N, L), espaco(N, RE)) :-
+  var(P),
+  append(RE, [P], Nova_RE),
+  espaco_fila(R, espaco(N, L), espaco(N, Nova_RE)).
+
+espaco_fila([P | R], espaco(N, L), espaco(N, []), D) :-
+  nonvar(P),
+  soma_na_direcao(P, D, N),
+  espaco_fila(R, espaco(N, L), espaco(N, [])).
+
+espaco_fila([_ | R], espaco(N, L), espaco(N, []), D) :-
+  espaco_fila(R, espaco(N, L), espaco(N, []), D).
