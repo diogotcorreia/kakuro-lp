0421e9230ca366a8b0fb68cc8a45fb9f3252c1c7
Diogo Correia <me@diogotc.com>
2021-04-30 15:43:53 +0100

    Add predicate 'permutacoes_soma'
    For some reason, setof/bagof did not work as expected,
    so findall + sort was used.

--- kakuro.pl
+++ kakuro.pl
@@ -2,3 +2,10 @@
 
 combinacoes_soma(N, Els, Soma, Combs) :-
   setof(X, (combinacao(N, Els, X), sum_list(X, Soma)), Combs).
+
+permutacoes_soma(N, Els, Soma, Perms) :-
+  combinacoes_soma(N, Els, Soma, Combs),
+  !,
+  % TODO why doesn't setof work here?
+  findall(X, (member(Comb, Combs), permutation(X, Comb)), Perms_Unsorted),
+  sort(Perms_Unsorted, Perms).
