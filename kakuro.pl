521234a6b397e7c8d6c1243c7c8949b2f3d8a741
Diogo Correia <me@diogotc.com>
2021-04-30 20:24:25 +0100

    Make espaco_fila/3 work with numbers in addition to vars

--- kakuro.pl
+++ kakuro.pl
@@ -22,23 +22,25 @@ espaco_fila(L, espaco(N, EL), D) :-
   espaco_fila(L, espaco(N, EL), espaco(N, []), D).
 
 % condicao de paragem: chegar ao final da lista
-espaco_fila([], espaco(N, EL), espaco(N, EL)).
+espaco_fila([], espaco(N, EL), espaco(N, EL)) :- 
+  length(EL, Length),
+  Length > 0.
 
 % condicao de paragem: encontrar um elemento que nao eh variável
 espaco_fila([P | _], espaco(N, EL), espaco(N, EL)) :- 
-  nonvar(P),
+  is_list(P),
   nonvar(N),
   length(EL, Length),
   Length > 0.
 
 espaco_fila([P | R], espaco(N, L), espaco(N, RE)) :-
-  var(P),
+  \+is_list(P),
   append(RE, [P], Nova_RE),
   espaco_fila(R, espaco(N, L), espaco(N, Nova_RE)).
 
 % espaco_fila/4
 espaco_fila([P | R], espaco(N, L), espaco(N, []), D) :-
-  nonvar(P),
+  is_list(P),
   soma_na_direcao(P, D, N),
   espaco_fila(R, espaco(N, L), espaco(N, [])).
 
